617650f7d92e4e60d5adbeb4a3a492433ad38b90
Amitai <git@amitai.ga>
2023-09-20 13:34:23 +0100

    anti-gravity

--- anti-gravity.py
+++ anti-gravity.py
@@ -1,15 +1,12 @@
 def on_item_interacted_slimeball():
-    jump()
-player.on_item_interacted(SLIMEBALL, on_item_interacted_slimeball)
-
-def jump():
     global time
-    time = gameplay.time_query(DAY_TIME)
     mobs.clear_effect(mobs.target(LOCAL_PLAYER))
+    time = gameplay.time_query(DAY_TIME)
     # more reliable than built-in `pause` function:
-    while gameplay.time_query(DAY_TIME) < time + 5:
+    while gameplay.time_query(DAY_TIME) < time + 3:
         continue
     mobs.apply_effect(LEVITATION, mobs.target(LOCAL_PLAYER), 600, 6)
+player.on_item_interacted(SLIMEBALL, on_item_interacted_slimeball)
 
 def on_item_interacted_netherite_ingot():
     mobs.clear_effect(mobs.target(LOCAL_PLAYER))
@@ -17,4 +14,4 @@ player.on_item_interacted(NETHERITE_INGOT, on_item_interacted_netherite_ingot)
 
 time = 0
 mobs.give(mobs.target(LOCAL_PLAYER), SLIMEBALL, 1)
-mobs.give(mobs.target(NEAREST_PLAYER), NETHERITE_INGOT, 1)
+mobs.give(mobs.target(LOCAL_PLAYER), NETHERITE_INGOT, 1)
